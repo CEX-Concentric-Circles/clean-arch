0ee189460fef9850ebb2ff9e196437b951f25d34
ram <maheshwariiram@gmail.com>
2024-07-15 16:20:02 +0200

    inventory
    + entity
    + repository
    + service
    + controller

---
+++ src/main/kotlin/concentric_circles/clean_architecture/service/InventoryService.kt
@@ -0,0 +1,64 @@
+package concentric_circles.clean_architecture.service
+
+//import concentric_circles.clean_architecture.model.dto.ProductDTO
+import concentric_circles.clean_architecture.model.entity.Inventory
+import concentric_circles.clean_architecture.model.entity.Product
+import concentric_circles.clean_architecture.repository.InventoryRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.util.UUID
+
+@Service
+class InventoryService {
+
+    @Autowired
+    lateinit var inventoryRepository: InventoryRepository
+
+    @Autowired
+    lateinit var productService: ProductService
+
+    fun addProductToInventory(product: Product, amount: Int = 1) {
+        val product = productService.getProductById(product.productId ?: UUID.randomUUID())
+
+        val inventory = product.productId?.let { inventoryRepository.findInventoryByProductId(it) }
+
+        if (inventory != null) {
+            inventory.productQuantity += amount
+        } else {
+            inventoryRepository.save(Inventory(inventoryId = UUID.randomUUID(), productId = product.productId, productQuantity = amount))
+        }
+    }
+
+    // i initially named the function removeFromInventory but i think decreaseProductStockFromInventory is more descriptive
+    // plus, removeFromInventory will be used to remove the whole stock of a product from the inventory
+    fun decreaseProductStockFromInventory(product: Product, amount: Int = 1): Boolean {
+        val product = productService.getProductById(product.productId ?: UUID.randomUUID())
+
+        val inventory = product.productId?.let { inventoryRepository.findInventoryByProductId(it) }
+
+        if (inventory != null) {
+            inventory.productQuantity -= amount
+            return true
+        } else {
+            // TODO: maybe throw a more descriptive exception?
+            throw Exception("Product not found in inventory")
+        }
+    }
+
+    fun checkIfProductIsInInventory(productID: UUID): Boolean {
+        return inventoryRepository.searchByProductId(productID)
+    }
+
+    fun removeProductFromInventory(product: Product): Boolean {
+        val inventory = product.productId?.let { inventoryRepository.findInventoryByProductId(it) }
+
+        if (inventory != null) {
+            inventoryRepository.delete(inventory)
+            return true
+        } else {
+            // TODO: maybe throw a more descriptive exception?
+            throw Exception("Product not found in inventory")
+        }
+    }
+
+}
---
+++ src/main/kotlin/concentric_circles/clean_architecture/web/InventoryController.kt
@@ -0,0 +1,27 @@
+package concentric_circles.clean_architecture.web
+
+import concentric_circles.clean_architecture.model.entity.Product
+import concentric_circles.clean_architecture.service.InventoryService
+import io.swagger.v3.oas.annotations.Operation
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class InventoryController (val inventoryService: InventoryService) {
+
+    @GetMapping("/inventory")
+    fun getInventory(): String {
+        return "This is the output from the inventory controller."
+    }
+
+    @Operation(summary = "Add a product to the inventory.")
+    @PostMapping("/addproducttoinventory")
+    fun addProductToInventory(@RequestBody product : Product, amount: Int = 1): ResponseEntity<out Any> {
+        val newProduct = inventoryService.addProductToInventory(product, amount)
+        return ResponseEntity.ok(newProduct)
+    }
+
+}
