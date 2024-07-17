e85c9c7cdc22c88aac061323c94a74e6205f5931
ram <maheshwariiram@gmail.com>
2024-07-17 11:19:03 +0200

    added a /getproduct mapping

--- src/main/kotlin/concentric_circles/clean_architecture/web/ProductController.kt
+++ src/main/kotlin/concentric_circles/clean_architecture/web/ProductController.kt
@@ -2,14 +2,19 @@ package concentric_circles.clean_architecture.web
 
 import concentric_circles.clean_architecture.model.entity.Product
 import concentric_circles.clean_architecture.service.ProductService
+import concentric_circles.clean_architecture.useCase.GetProductUseCase
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
-class ProductController (val productService: ProductService) {
+class ProductController (
+    val productService: ProductService,
+    val getProductUseCase: GetProductUseCase
+) {
 
     @GetMapping("/allproducts")
     fun getAllProducts(): ResponseEntity<List<Product>> {
@@ -19,6 +24,11 @@ class ProductController (val productService: ProductService) {
     @PostMapping("/addproduct")
     fun addProduct(@RequestBody product: Product):ResponseEntity<out Any> {
         return ResponseEntity.ok().body(productService.createProduct(product))
+    }
+
+    @GetMapping("/getproduct")
+    fun getProductById(@RequestBody productId: UUID): ResponseEntity<out Any>{
+        return ResponseEntity.ok().body(getProductUseCase.getProductById(productId))
     }
 
 }
