e7f6d1f28362008651627b78fcaee4b95fc9fa7d
ram <maheshwariiram@gmail.com>
2024-07-31 17:55:12 +0530

    feat: findByProductID in the orderRepo

--- src/main/kotlin/concentric_circles/clean_architecture/repository/OrderRepository.kt
+++ src/main/kotlin/concentric_circles/clean_architecture/repository/OrderRepository.kt
@@ -6,6 +6,5 @@ import org.springframework.data.repository.CrudRepository
 import java.util.*
 
 interface OrderRepository: CrudRepository<Order, UUID> {
-    // this might be needed in the future? idk
-//    fun findOrdersContainingAProduct(product: Product): Order?
+    fun findOrdersByProductId(productId: UUID): Order?
 }
