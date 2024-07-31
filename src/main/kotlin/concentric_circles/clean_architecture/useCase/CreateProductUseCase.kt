4a009e35708882a646255308f58e940093d394d3
ram <maheshwariiram@gmail.com>
2024-07-31 23:20:43 +0530

    fix: if a duplicate product is created, increase the product's quantity in the inventory
    earlier, creating a duplicate product would throw an error like 'product already exists'.

--- src/main/kotlin/concentric_circles/clean_architecture/useCase/CreateProductUseCase.kt
+++ src/main/kotlin/concentric_circles/clean_architecture/useCase/CreateProductUseCase.kt
@@ -7,14 +7,19 @@ import org.springframework.stereotype.Service
 
 @Service
 class CreateProductUseCase (
-    var productRepository: ProductRepository
+    var addProductToInventoryUseCase: AddProductToInventoryUseCase
 ) {
+
+    @Autowired
+    lateinit var productRepository: ProductRepository
 
     fun createProduct(product: Product): Product {
         val existingProduct = productRepository.findProductByName(product.name)
 
         if (existingProduct != null) {
-            throw Exception("Product '${product.name}' already exists. If you want to, you can modify the amount of the product in the inventory.")
+            addProductToInventoryUseCase.addProductToInventory(existingProduct.productId)
+            println("Product '${product.name}' already exists. Added this to the inventory.")
+            return existingProduct
         }
 
         val createdProduct = productRepository.save(product)
