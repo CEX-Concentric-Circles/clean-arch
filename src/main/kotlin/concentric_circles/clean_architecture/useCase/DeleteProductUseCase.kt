4af4ab0a9ee9fdfc46546f9da99770ce98bb236e
ram <maheshwariiram@gmail.com>
2024-07-31 23:21:46 +0530

    fix: search by productId instead of productName while deleting a product

--- src/main/kotlin/concentric_circles/clean_architecture/useCase/DeleteProductUseCase.kt
+++ src/main/kotlin/concentric_circles/clean_architecture/useCase/DeleteProductUseCase.kt
@@ -9,21 +9,24 @@ import java.util.UUID
 @Service
 class DeleteProductUseCase (
     val productRepository: ProductRepository,
-    val productExistsUseCase: CheckIfProductExistsUseCase
+    val productExistsUseCase: CheckIfProductExistsUseCase,
+    val getProductUseCase: GetProductUseCase
 ) {
 
     @Transactional
     fun deleteProduct (product: Product): Any {
 
-        val existingProduct = productExistsUseCase.checkIfProductExists(product.name)
+        val searchedProduct = getProductUseCase.getProductById(product.productId)
             ?: throw Exception("Product with name:'${product.name}' does not exist.")
+
+        // TODO: also delete the inventory related to this product if/once the product is deleted.
 
 //        val existingProduct = productRepository.findById(product.productId)
 //            ?: throw Exception("Product with ID:'${product.productId}' does not exist.")
 
 //        if(existingProduct == null) throw Exception("Product with ID:'${product.productId}' does not exist.")
 //        else
-        return productRepository.delete(existingProduct)
+        return productRepository.delete(searchedProduct)
     }
 
 }
