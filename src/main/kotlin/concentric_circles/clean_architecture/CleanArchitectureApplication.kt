fe97f052650ee21dc50b44c3aeb0de8385739eba
ram <maheshwariiram@gmail.com>
2024-09-03 01:25:58 +0530

    fix: added ConfigPropertiesScan

--- src/main/kotlin/concentric_circles/clean_architecture/CleanArchitectureApplication.kt
+++ src/main/kotlin/concentric_circles/clean_architecture/CleanArchitectureApplication.kt
@@ -1,10 +1,12 @@
 package concentric_circles.clean_architecture
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class CleanArchitectureApplication
+@ConfigurationPropertiesScan
+open class CleanArchitectureApplication
 
 fun main(args: Array<String>) {
 	runApplication<CleanArchitectureApplication>(*args)
